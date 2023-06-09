8b532fbe3c2849b910cbcdc7261c45cf15b066a3
MEROU-Clemence <clemence.merou@gmail.com>
2023-06-09 13:55:45 +0200

    start-mapbox-js-objet

---
+++ src/App/App.js
@@ -0,0 +1,15 @@
+
+class App {
+
+    start() {
+        console.log( 'App démarrée...');
+    }
+
+    close(){
+        console.log('app fermée...');
+    }
+}
+
+const app = new App();
+
+export default app;
